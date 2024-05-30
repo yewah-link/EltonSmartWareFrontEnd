259bf535fb025cdbb194e444a10e31f8f89f843f
Abraham Yewa <110853073+yewah-link@users.noreply.github.com>
2024-05-30 19:21:58 +0300

    Contains dashboards update

--- EltonSmartWareFrontEnd/src/app/customer/customer.module.ts
+++ EltonSmartWareFrontEnd/src/app/customer/customer.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
-    CustomerComponent
+    CustomerComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
